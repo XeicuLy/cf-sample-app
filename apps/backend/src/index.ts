bbc2c9609c26e3025c14d3b0c82c24b59772f2d1
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-12-05 23:21:08 +0900

    🌱 feat: シードユーザー作成API追加

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,9 @@
+import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { auth } from './lib/better-auth';
 import { corsMiddleware } from './middleware/cors';
+import { apiKeyAuthMiddleware } from './middleware/secret-key';
+import { createSeedUserRouteHandler } from './routes/add-seed-user/post';
 
 const app = new OpenAPIHono<{
   Bindings: CloudflareBindings;
@@ -10,4 +13,24 @@ app.use('/*', corsMiddleware);
 
 app.on(['GET', 'POST'], '/api/v1/auth/*', (c) => auth(c.env).handler(c.req.raw));
 
-export default app;
+app.use('/api/v1/secret/*', apiKeyAuthMiddleware);
+export const routes = app.route('/', createSeedUserRouteHandler);
+
+routes
+  .doc('/api', {
+    openapi: '3.0.0',
+    info: {
+      title: 'API',
+      version: '1.0.0',
+    },
+  })
+  .get(
+    '/docs',
+    swaggerUI<{ Bindings: CloudflareBindings }>({
+      url: '/api',
+    }),
+  );
+
+export type ApiType = typeof routes;
+
+export default routes;
