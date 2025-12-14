43762eb1524ff12a16fff38e3ba4a0450649a31c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-12-14 18:10:04 +0900

    ✨ feat: フロントエンド画面追加

--- apps/frontend/src/lib/hono-client.ts
+++ apps/frontend/src/lib/hono-client.ts
@@ -9,7 +9,16 @@ const client = (baseUrl: string) =>
     },
   }).api.v1;
 
+// api/v1/secure/helloの post を呼び出す関数を作成
 export const $getHello = client(import.meta.env.VITE_BACKEND_URL).secure.hello.$post;
-
 export type GetHelloRequest = InferRequestType<typeof $getHello>;
 export type GetHelloResponse = InferResponseType<typeof $getHello>;
+
+// api/v1/secure/products の get を呼び出す関数を作成
+export const $getAllCategoriesAndProducts = client(import.meta.env.VITE_BACKEND_URL).secure.category.$get;
+export type getAllCategoriesAndProductsResponse = InferResponseType<typeof $getAllCategoriesAndProducts>;
+
+// api/v1/secure/products の delete  を呼び出す関数を作成
+export const $deleteCategory = client(import.meta.env.VITE_BACKEND_URL).secure.category[':categoryId'].$delete;
+export type DeleteCategoryRequest = InferRequestType<typeof $deleteCategory>;
+export type DeleteCategoryResponse = InferResponseType<typeof $deleteCategory>;
